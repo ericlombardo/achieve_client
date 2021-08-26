f64fff460d4d9a7e8fc44b8fcd715ae4367d47b4
Eric <eric.m.lombardo@gmail.com>
2021-08-26 19:06:35 -0400

    Pare down ActivityLogTracker to bare framework

--- src/containers/ActivityLogTracker.js
+++ src/containers/ActivityLogTracker.js
@@ -1,20 +1,10 @@
 import React from 'react'
-import { Button, Grid } from 'semantic-ui-react'
-
+import { Button } from 'semantic-ui-react'
 const ActivityLogTracker = () => {
   
-  const handleClick = () => {
-    debugger
-    // find what it is and then execute
-  }
   
   return (
-    <Grid.Column width={10}>
-        <Button onClick={handleClick} id="start">Start</Button>
-        <Button id="stop">Stop</Button>
-        <Button id="commit">Commit</Button>
-        <Button id="discard">Discard</Button>
-    </Grid.Column>
+    <div></div>
   )
 }
 
