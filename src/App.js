8aae1d1ab04055e079244a44e84d1319290d694f
Eric <eric.m.lombardo@gmail.com>
2021-08-28 14:21:38 -0400

    Switch App component from class to function

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
 
-// import files to accessing
 import Home from './components/Home'
 import GoalsNew from './components/GoalsNew'
 import ActivityLog from './components/ActivityLog'
 
-class App extends Component {
-  render() {
-    return (
-      <Router> {/* renders router component to handle different routes */}
-        <div id="navbar">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/goals/new">New Goal</NavLink>
-          <NavLink to="/activitylog">Activity Log</NavLink>
-        </div>
-        <div id="routes">
-          <Route exact path="/">
-            <Home />
-          </Route>
+const App = () => {
+  return (
+    <Router> {/* renders router component to handle different routes */}
+      
+      {/* render NavLink components for each route needed */}
+      <div id="navbar">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/goals/new">New Goal</NavLink>
+        <NavLink to="/activitylog">Activity Log</NavLink>
+      </div>
+      
+      {/* define what components to render based on route paths */}
+      <div id="routes">
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-          <Route exact path="/goals/new">
-            <GoalsNew />
-          </Route>
+        <Route exact path="/goals/new">
+          <GoalsNew />
+        </Route>
 
-          <Route exact path="/activitylog"> 
-            <ActivityLog />
-          </Route>
-        </div>
-      </Router>
-    )
-  }
+        <Route exact path="/activitylog"> 
+          <ActivityLog />
+        </Route>
+      </div>
+    </Router>
+  )
 }
 
 export default App
