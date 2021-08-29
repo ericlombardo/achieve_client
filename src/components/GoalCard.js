fcc6629e51d463c65b6c6d2a6028c751aa2fcca9
Eric <eric.m.lombardo@gmail.com>
2021-08-29 03:05:14 -0400

    Set up GoalContainer and GoalCard to display goals

--- src/components/GoalCard.js
+++ src/components/GoalCard.js
@@ -1,10 +1,19 @@
+import React from 'react'
 
 const GoalCard = (props) => {
+  const {title, why, durationNumber, durationUnit, durationEnd, goalVerb, goalNumber, goalUnit, timeSpent} = props.goal
   return (
-    <div id="goal-card" className="border card">
-      {/* display goal info */}
-      <h1>Goal Card</h1>
-    </div>
+    <>
+      <h1>{title}</h1>
+      <p>{why}</p>
+      <p>{durationNumber}</p>
+      <p>{durationUnit}</p>
+      <p>{durationEnd}</p>
+      <p>{goalVerb}</p>
+      <p>{goalNumber}</p>
+      <p>{goalUnit}</p>
+      <p>{timeSpent}</p>
+    </>
   )
 }
 
--- src/components/GoalContainer.js
+++ src/components/GoalContainer.js
@@ -1,19 +1,31 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchGoals } from '../actions/goalActions'
 import GoalCard from './GoalCard'
-// connect to store
-// get all goals
-// map through each and display GoalCard
 
 const GoalContainer = () => {
-  return (
-    <div id="goal-container" className="border center">
-      {/* get all goals from global store and map each one to GoalCard */}
-      <GoalCard />
-      <GoalCard />
-      <GoalCard />
-      <GoalCard />
-      <GoalCard />
-      <GoalCard />
+  
+  const dispatch = useDispatch()  // gives access to dispatch method
+  const goals = useSelector(state => state.goals.goals) // assigns store value to goals variable
+  const loading = useSelector(state => state.goals.loading) // assigns store value to loading variable
+  
+  useEffect(() => {  // runs side effect
+    dispatch(fetchGoals()) // calls dispatch with fetchGoals action
+  }, [dispatch]) // renders whenever dispatch changes
+  
+  if (loading) { return null } 
+  
+  return ( 
+    <div id="goals">
+      {goals.map(goal => { // map through all goals
+        return ( // return div with GoalCard component for each
+          <div id="goal-card" className="border card" key={goal.id}>
+            <GoalCard goal={goal} /> {/* pass in goal instance through props */}
+          </div> 
+        )
+      })}
     </div>
   )
 }
-export default GoalContainer
+
+export default GoalContainer 
