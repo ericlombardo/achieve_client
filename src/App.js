b36bf9fbba17f1e97c218538de4d3595ce55e5fe
Eric <eric.m.lombardo@gmail.com>
2021-09-05 19:50:11 -0400

    Format styling on home page

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import NavBar from './components/display/NavBar'
 const App = () => { // uses Router to direct routes to Home, Goals, New Goal
   return (
     <div className="container">
-      <Router> {/* renders router component to handle different routes */}
-        {/* define what components to render based on route paths */}
-        {/* bg-img bg-center bg-repeat-y bg-cover w-screen h-auto this works for form resize */}
+      <Router> {/* renders router component to handle different Route paths */}
+          
           <Route exact path="/">
+            {/* set background each time to handle resizing of forms */}
             <div id="home" className="bg-img bg-cover bg-center bg-no-repeat w-screen min-h-screen">
               <HomePage />
             </div>
--- src/components/display/HomePage.js
+++ src/components/display/HomePage.js
@@ -5,21 +5,17 @@ import Button from './Button'
 
 const HomePage = () => {
   return (    
-    <div className=" grid grid-rows-2 gap-2 max-w-sm pt-44 ml-80">
-      <div>
+    <div className="flex justify-center items-center w-screen h-screen grid grid-rows-2 ">
+      <div className="max-w-md grid gap-2">
       
         <QuoteContainer /> {/* render container with logic and render quote component*/}
-      
-      </div>
-      <div className="mx-auto">
-
         <Link to="/goals">  {/* create link for button component */}
           <Button 
             text="TRACK YOUR GOALS" 
-            classes="bg-orange text-white f-title text-center p-2 w-48 h-auto shadow-2xl rounded-2xl"
+            classes="bg-orange text-white f-title text-center p-2 w-48 h-auto text-2xl md:text-3xl shadow-2xl rounded-2xl"
           />
         </Link>
-        
+      
       </div>
     </div>
   )
