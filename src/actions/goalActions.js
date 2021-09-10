3ddfc2d4f5b8d4e0671eaaa836dc9b8fe553c738
Eric <eric.m.lombardo@gmail.com>
2021-09-09 20:32:08 -0400

    Fix error for updating milestones

--- src/actions/goalActions.js
+++ src/actions/goalActions.js
@@ -23,13 +23,15 @@ export const updateGoal = (updatedGoal) => { // takes in updatedGoal and updates
   }}
 
 export const updateGoalServer = (newGoal) => {  // sends newGoal to server as patch
-  fetch(`http://localhost:3000/goals/${newGoal.id}`, { // form id is set to goal
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({goal: newGoal})}
-  )
+  return (dispatch) => {
+    fetch(`http://localhost:3000/goals/${newGoal.id}`, { // form id is set to goal
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({goal: newGoal})}
+    )
+  }
 }
   
 export const CreateGoal = (goal) => {  // assigns conditional title, posts to server, adds to redux
@@ -62,7 +64,7 @@ export const CreateGoal = (goal) => {  // assigns conditional title, posts to se
     }
 }
 
-export const removeGoal = id => { // deletes goal from server and then updates store
+export const removeGoal = id => { // deletes goal from server and then updates
   fetch(`http://localhost:3000/goals/${id}`, {
     method: 'DELETE',
     headers: {
